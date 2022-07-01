ae759aa6468a7454eea6f7585364b34b0ed0d10c
Jader Adriel <jaderadriel7@gmail.com>
2022-06-30 23:33:50 -0300

    - adicao de views (listRedactions, create, view), - adicao de urls para views, - mudanca nos temmplates

--- REDIF/REDIF/urls.py
+++ REDIF/REDIF/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin", admin.site.urls),
-    path("Redif/", include("RedifApp.urls")),
+    path("redif/", include("RedifApp.urls")),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- REDIF/RedifApp/urls.py
+++ REDIF/RedifApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path("", views.teste),
+    path("listar/", views.listRedactions, name='listRedactions'),
+    path("create/",  views.create, name='create'),
+    path('view/<int:id>/', views.view, name='read'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
 ]
