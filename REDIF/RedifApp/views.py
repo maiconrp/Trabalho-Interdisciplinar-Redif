93f19ae566c6551dbbae3655d1e34a420244f37a
maicon15rp <60708311+maicon15rp@users.noreply.github.com>
2022-07-02 04:41:58 -0300

    Refatoramento View.py
    Removi coisas desnecessárias e reduzi o códigp

--- REDIF/RedifApp/views.py
+++ REDIF/RedifApp/views.py
@@ -1,106 +1,84 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import HttpResponseRedirect, render
 
 from django.contrib.auth.models import User
-from matplotlib.style import context
-from requests import request
 from RedifApp.forms import RedacaoForm
 from RedifApp.models import Redacao
 
 from datetime import datetime
 
-def listarRedacao(request):
+def home(request):
+    return render(request, "home.html")
+
+def ListarRedacao(request):
     Redacoes = Redacao.objects.all()
+
     context = {
-        "Redacao": Redacoes,
-        'Logado': None,
-        'Usuario' : None
-    }
+        "Redacao": Redacoes
+        }
 
     if User.is_authenticated:
         try:
             Usuario = request.user.id
-            context['Usuario'] = User.objects.get(pk=Usuario)
-            context['Logado'] = True
+            context ["Usuario"] = User.objects.get(pk=Usuario)
+            context ["Logado"]  = True
+
         except: pass
 
-    return render(request,'redacao/listar.html', context)
+    return render(request,"Redacao/listar.html", context)
 
 @login_required
-def criarRedacao(request):
-
-    if request.method == 'GET':
-        form = RedacaoForm()
-        context = {
-        'form' : form
-        }
-        return render(request, 'redacao/criar.html', context=context)
-    
-    form = RedacaoForm(request.POST)
+def CriarRedacao(request):
     user = request.user.id
+    form = RedacaoForm(request.POST)
 
-    if form.is_valid():
-        titulo = form.cleaned_data['titulo']
-        area = form.cleaned_data['area']
-        tema = form.cleaned_data['tema']
-        conteudo = form.cleaned_data['conteudo']
-        comentario = form.cleaned_data['comentario']
-        data_criacao = datetime.now()
-        fk_autor = User.objects.get(pk=user)
+    if request.method == "POST":
+        if form.is_valid():
+            nova_redacao = form.save(commit=False)
 
-        nova_redacao = Redacao(
-            titulo= titulo, 
-            area= area, 
-            tema= tema, 
-            conteudo= conteudo, 
-            comentario= comentario, 
-            data_criacao= data_criacao, 
-            fk_autor= fk_autor,
-        )
-        nova_redacao.save()
-        
-    
+            nova_redacao.data_criacao = datetime.now()
+            nova_redacao.fk_autor = User.objects.get(pk=user)
+
+            nova_redacao.save()
+            return HttpResponseRedirect("/redif")
+        else:
+            form = RedacaoForm()
+
     context = {
-        'form' : form
+        "form": form
     }
 
-    return render(request, 'redacao/detalhar.html', context=context)
+    return render(request, "Redacao/criar.html", context)
 
-
-def detalharRedacao(request, id):
+def DetalharRedacao(request, id):
     redacao = Redacao.objects.get(pk=id)
     autor = redacao.fk_autor.username
-    context = {
-        'Redacao' : redacao,
-        'autor' : autor
-    }
 
-    return render(request, 'redacao/detalhar.html', context)
+    context = {
+        "Redacao" : redacao,
+        "autor"   : autor
+    }
+    return render(request, "Redacao/detalhar.html", context)
 
 @login_required
-def editarRedacao(request, id):
+def EditarRedacao(request, id):
     redacao = Redacao.objects.get(pk=id)
-
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RedacaoForm(request.POST, instance=redacao)
-
         if form.is_valid():
             form.save()
-            return redirect('/redif/listar')
-    
-    form = RedacaoForm(instance=redacao)
+            return HttpResponseRedirect("/redif/")
+    else:
+        form = RedacaoForm()
+
     context = {
-        'form' : form,
-        'id' : id,
+        "form" : form,
+        "id"   : id,
     }
 
-    return render(request, 'redacao/editar.html', context=context)
-
-
+    return render(request, "Redacao/editar.html", context)
 
 @login_required
-def deletarRedacao(request, id):
+def DeletarRedacao(request, id):
     Redacao.objects.get(pk=id).delete()
-    return redirect('/redif/listar')
-
+    return HttpResponseRedirect("/redif")
