f59e352daa82ec97ba1bf262063b6ca957a91172
maicon15rp <60708311+maicon15rp@users.noreply.github.com>
2022-07-02 04:56:02 -0300

    Acrecismo da view home e formatação do código

--- REDIF/RedifApp/urls.py
+++ REDIF/RedifApp/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path("listar/", views.listarRedacao, name='listarRedacao'),
-    path("criar/",  views.criarRedacao, name='criarRedacao'),
-    path('detalhar/<int:id>/', views.detalharRedacao, name='detalharRedacao'),
-    path('editar/<int:id>/', views.editarRedacao, name='editarRedacao'),
-    path('deletar/<int:id>/', views.deletarRedacao, name='deletarRedacao'),
+urlpatterns  =  [
+    path("home",                views.home,             name = 'Tela inicial'),
+    path("criar/",              views.CriarRedacao,     name = 'criarRedacao'),
+    path("",                    views.ListarRedacao,    name = 'listarRedacao'),
+    path('editar/<int:id>/',    views.EditarRedacao,    name = 'editarRedacao'),
+    path('detalhar/<int:id>/',  views.DetalharRedacao,  name = 'detalharRedacao'),
+    path('deletar/<int:id>/',   views.DeletarRedacao,   name = 'deletarRedacao'),
 ]
