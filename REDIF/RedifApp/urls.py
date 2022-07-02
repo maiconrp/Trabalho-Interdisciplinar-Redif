9df9cacc1b7277a9c8e974d845c1145b2d41e8da
maicon15rp <60708311+maicon15rp@users.noreply.github.com>
2022-07-02 16:11:46 -0300

    Correção de nomenclaturas

--- REDIF/RedifApp/urls.py
+++ REDIF/RedifApp/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns  =  [
     path("home",                views.home,             name = 'Tela inicial'),
-    path("criar/",              views.CriarRedacao,     name = 'criarRedacao'),
-    path("",                    views.ListarRedacao,    name = 'listarRedacao'),
-    path('editar/<int:id>/',    views.EditarRedacao,    name = 'editarRedacao'),
-    path('detalhar/<int:id>/',  views.DetalharRedacao,  name = 'detalharRedacao'),
-    path('deletar/<int:id>/',   views.DeletarRedacao,   name = 'deletarRedacao'),
+    path("criar/",              views.criarRedacao,     name = 'criarRedacao'),
+    path("",                    views.listarRedacao,    name = 'listarRedacao'),
+    path('editar/<int:id>/',    views.editarRedacao,    name = 'editarRedacao'),
+    path('detalhar/<int:id>/',  views.detalharRedacao,  name = 'detalharRedacao'),
+    path('deletar/<int:id>/',   views.deletarRedacao,   name = 'deletarRedacao'),
 ]
