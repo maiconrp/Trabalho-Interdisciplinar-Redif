b953182fbfbf05765c8a1e9996060c3c617e0986
Victor Fonteles <sedrfwfefe@gmail.com>
2022-07-07 10:23:33 -0300

    Merge branch 'master' of https://github.com/maicon15rp/Trabalho-Interdisciplinar-Redif

--- REDIF/RedifApp/models.py
+++ REDIF/RedifApp/models.py
@@ -2,44 +2,12 @@ import datetime
 from tkinter import ALL
 from django.db import models
 from django.contrib.auth.models import User
-
+from accounts.models import Usuario
 #adicionado la no settings.py
 
 from multiselectfield import MultiSelectField
 # Create your models here.   
 
-#Crio classe Usuário e faço herdar de User, restando apenas adcionar os campos especificos
-class Usuario(User):
-    
-    TITULACAO_CHOICES = (
-        ("1", "Fundamental 2 ao 8°ano"),
-        ("2", "Cursando 9° ano"),
-        ("3", "Cursando Ensino Médio"),
-        ("4", "Ensino Médio Completo"),
-        ("5", "Ensino Médio Incompleto"),
-        ("6", "Cursando Ensino Superior"),        
-        ("7", "Ensino Superior Completo")
-    )
-
-    CONDICAO_CHOICES = {
-        ("P", "Professor"),
-        ("A", "Aluno")
-    }
-
-    #verifica a titulacao do usuario
-    titulacao = models.CharField(
-        max_length=1, 
-        choices= TITULACAO_CHOICES,
-        blank=False, 
-        null = False,
-    )
-    #verifica se é aluno ou professor
-    condicao = models.CharField(
-        max_length=1, 
-        choices= CONDICAO_CHOICES,
-        blank=False, 
-        null = False,
-    )
 
 class Redacao(models.Model):
     #area como um campo de multipla escolha
