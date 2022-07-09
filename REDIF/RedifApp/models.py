49378e3c73482ecb89ebb3e52ac66811c6abb8c7
jaderAdriel <jaderadriel7@gmail.com>
2022-07-09 13:56:29 -0300

    model para filtro

--- REDIF/RedifApp/models.py
+++ REDIF/RedifApp/models.py
@@ -89,14 +89,46 @@ class Avaliacao(models.Model):
     comentario = models.TextField(
         max_length=300, 
     )
-    nota = models.SmallIntegerField()
+    nota = models.SmallIntegerField(max_length=1000)
 
     data_criacao = models.DateTimeField(
         default=datetime.date.today
     )
     def __str__(self):
         return "Avaliador: {} - Redação Avaliada: {} - Nota dada: {}\n".format(self.usuario.username, self.redacao.titulo, self.nota)
-    
+
+
+class Filtro(models.Model):
+    #area como um campo de multipla escolha
+    AREA_CHOICES = (
+        ("CET","Ciencias Exatas e da Terra"),
+        ("CB","Ciências Biológicas"),
+        ("CSA", "Ciências Sociais Aplicadas"),
+        ("E","Engenharias"),
+        ("LLA","Linguística, Letras e Artes"),        
+        ("CH", "Ciências Humanas")
+    )
+    titulo = models.CharField(
+        max_length = 45, 
+        blank = True,
+        null = True,
+    )
+
+    area = MultiSelectField(
+        max_length=40, 
+        max_choices=10,
+        choices= AREA_CHOICES,
+        blank = True,
+        null = True,
+        default=  ["CET", "CB", "CSA", "E", "LLA", "CH",]
+    )
+
+    tema = models.CharField(
+        blank = True,
+        null = True,
+        max_length=45,
+        default= 'All'
+)
 
 
 
