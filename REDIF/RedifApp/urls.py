46b59a279504083002a01cc8200745238ce916ef
jaderAdriel <jaderadriel7@gmail.com>
2022-07-09 18:00:43 -0300

    .

--- REDIF/RedifApp/urls.py
+++ REDIF/RedifApp/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns  =  [
     path("home",                            views.home,             name = 'Tela inicial'),
     path("criar/",                          views.criarRedacao,     name = 'criarRedacao'),
+    path("perfil/<str:usuario>/",           views.perfilUsuario,    name='perfilUsuario'),
 
     path("",                                views.listarRedacao,    name = 'listarRedacao'),
-    path("listar/",                                views.listarRedacao,    name = 'listarRedacao'),
+    path("listar/",                         views.listarRedacao,    name = 'listarRedacao'),
     path('editar/<int:id>/',                views.editarRedacao,    name = 'editarRedacao'),
     path('detalhar/<int:id>/',              views.detalharRedacao,  name = 'detalharRedacao'),
     path('deletar/<int:id>/',               views.deletarRedacao,   name = 'deletarRedacao'),
