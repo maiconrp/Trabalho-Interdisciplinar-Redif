1f9216f6028bdd374165c75132545cb1514f2737
Maicon Robert <maiconlk2321@gmail.com>
2022-07-09 13:01:46 -0300

    implemenação de modal html

--- REDIF/static/js/script.js
+++ REDIF/static/js/script.js
@@ -194,3 +194,8 @@
     });
 
 })(jQuery);
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
